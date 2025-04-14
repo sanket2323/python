35872bf3e56d1b155ad7786ddadd21f512864742
sanket2323 <sanketmane2323@gmail.com>
2025-04-15 01:07:34 +0530

    questions n answers

---
+++ Functions/question.py
@@ -0,0 +1,15 @@
+#1
+
+def sequre (number):
+    return number ** 2
+
+value = sequre(3)
+print(value)
+
+#2
+
+def lenght_of_string(string):
+    return len(string)
+
+value_string = lenght_of_string("sanket")
+print(value_string)
---
+++ List/question.py
@@ -0,0 +1,13 @@
+#1
+city_list = ["Banglore","Mumbai","Solapur","Madhapur","Akkalkot"]
+print(city_list[2])
+
+#2
+city_list.append("Jaipur")
+print(city_list)
+
+city_list.remove("Solapur")
+print(city_list)
+
+#3
+tuppple =(1,2,3,5)

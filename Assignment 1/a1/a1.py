8c2e4d6bda0147ac4b499e4412df09aad16078e2
sanket2323 <sanketmane2323@gmail.com>
2026-04-05 16:06:08 +1000

    assingment task

---
+++ Assignment 1/a1/a1.py
@@ -0,0 +1,22 @@
+# DO NOT modify or add any import statements
+from support import *
+
+ALL_WORDS = load_words("words.txt")
+
+# Name:
+# Student Number: 
+# Favorite Word: 
+# -----------------------------------------------------------------------------
+
+# Define your functions here
+
+load_words()
+
+def main() -> None:
+    pass
+
+def has_won() -> bool: 
+    
+
+if __name__ == "__main__":
+    main()

fb353948e5f5d5819d2f51b8ea7ef159db9e4938
sanket2323 <sanketmane2323@gmail.com>
2026-05-27 23:36:48 +1000

    extract number function added

--- Exam Prep/week1.py
+++ Exam Prep/week1.py
@@ -21,3 +21,24 @@ def swap_number(x, y):
 
 
 print(swap_number(100, 200))
+
+print(2 + 3 * 4 ** 2)
+
+num = 572
+
+def extract_number(number):
+
+    hund_place = number // 100
+    tenth_place = (number // 10) % 10
+    first_place = number % 10
+
+    return hund_place, tenth_place,  first_place
+
+print(extract_number(num))
+
+
+
+
+
+
+

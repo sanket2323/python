4be1c07106faab9143e1a529478da5c5cea5d18a
sanket2323 <sanketmane2323@gmail.com>
2026-05-27 22:48:33 +1000

    swap two numbers code added

--- Exam Prep/week1.py
+++ Exam Prep/week1.py
@@ -1,11 +1,23 @@
 def find_remainder(a, b):
     return [a // b, a % b]
+
+
 print(find_remainder(6, 3))
 
 
 def second_largest_number(a, b, c):
     return a + b + c - max(a, b, c) - min(a, b, c)
+
+
 print(second_largest_number(20, 30, 40))
 
 
+def swap_number(x, y):
+    x = x + y
+    y = x - y
+    x = x - y
 
+    return x, y
+
+
+print(swap_number(100, 200))

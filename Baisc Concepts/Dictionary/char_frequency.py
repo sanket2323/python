3d91f11f073ead52349963c561cf454074704539
sanket2323 <sanketmane2323@gmail.com>
2026-05-31 17:51:29 +1000

    char frequency dictionary problem

---
+++ Baisc Concepts/Dictionary/char_frequency.py
@@ -0,0 +1,15 @@
+def char_frequency(text:str):
+
+    frequency = {}
+
+    for char in text.lower():
+
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+
+    return frequency
+
+char_frequency('sanket')
+print(char_frequency('sankets'))

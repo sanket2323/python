27cfaeeca3d62ab4346aeddd7ec7fec48e609c94
sanket2323 <sanketmane2323@gmail.com>
2026-05-31 18:16:41 +1000

    combine dictionary problem

--- Baisc Concepts/Dictionary/combine.py
+++ Baisc Concepts/Dictionary/combine.py
@@ -1,13 +1,11 @@
 def combine(dict1, dict2):
-    ans ={}
+    ans = {}
     for key in dict1:
 
         if key in dict2:
             ans[key] = sum(dict1[key]) + sum(dict2[key])
 
-
     return ans
 
 
 print(combine({1: [2], 4: [5, 6]}, {4: [8]}))
-

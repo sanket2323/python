c399320a1d3cdec3ef92c45a937a87930281758f
sanket2323 <sanketmane2323@gmail.com>
2026-05-31 17:51:15 +1000

    flatten matrix problem

---
+++ Exam Prep/flatten_matrix.py
@@ -0,0 +1,15 @@
+matrix = [
+    [1, 2, 3],
+    [2, 2, 5]
+]
+
+flatten_list = []
+
+for i in range(len(matrix)):
+
+    flatten_list.extend(matrix[i])
+
+print(flatten_list)
+
+
+print("hello world")
